681692fb34f46a574772cb66762f4c337e92e2cb
MatijaxD <93matija@gmail.com>
2016-05-24 10:02:18 +0200

    Create rss_parser.py

---
+++ rss_parser.py
@@ -0,0 +1,22 @@
+"""Gets the torrent RSS feed and parses it."""
+
+import re
+import requests
+import html
+
+def getRss():
+	try:
+		feed = requests.get('https://www.nyaa.se/?page=rss&user=64513')
+		text = html.unescape(feed.text)
+
+		try:
+			titles=re.findall(r'<title>\[HorribleSubs\] (.*?).mkv</title>', text)
+			links=re.findall(r'<link>(.*?)</link>', text)
+			
+			return (titles[0], links[1])
+			
+		except Exception as e:
+			print(str(e))	
+		
+	except Exception as e:
+		print(str(e))
