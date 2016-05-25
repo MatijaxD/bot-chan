1e3fcca48a5a0548164ce96566e4a726fbd35126
MatijaxD <93matija@gmail.com>
2016-05-25 10:38:08 +0200

    switched tabs with spaces

--- bot-chan.py
+++ bot-chan.py
@@ -30,48 +30,48 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	elif message.channel.is_private:
-		tmp = await client.send_message(message.channel, 'kys')
-		await client.edit_message(tmp, 'lol')
-	elif message.content.startswith('!crop') and len(message.attachments)>0:
-                source = tinify.from_url(message.attachments[0]['url'])
-                resized = source.resize(
-                    method="cover",
-                    width=128,
-                    height=128
-                )
-                resized.to_file("img.png")
-                await client.send_file(message.channel, "img.png")
-	elif message.content.startswith('!crop') and len(message.embeds)>0:
-                source = tinify.from_url(message.embeds[0]['url'])
-                resized = source.resize(
-                    method="cover",
-                    width=128,
-                    height=128
-                )
-                resized.to_file("img.png")
-                await client.send_file(message.channel, "img.png")
+    if message.author == client.user:
+        return
+    elif message.channel.is_private:
+        mp = await client.send_message(message.channel, 'kys')
+        await client.edit_message(tmp, 'lol')
+    elif message.content.startswith('!crop') and len(message.attachments)>0:
+        source = tinify.from_url(message.attachments[0]['url'])
+        resized = source.resize(
+            method="cover",
+            width=128,
+            height=128
+        )
+        resized.to_file("img.png")
+        await client.send_file(message.channel, "img.png")
+    elif message.content.startswith('!crop') and len(message.embeds)>0:
+        source = tinify.from_url(message.embeds[0]['url'])
+        resized = source.resize(
+            method="cover",
+            width=128,
+            height=128
+        )
+        resized.to_file("img.png")
+        await client.send_file(message.channel, "img.png")
 
 @client.event
 async def on_member_remove(server):
-	if (client.user not in bans):
-		msg = '**{0.name}** left or was kicked from the server.'.format(server)
-		await client.send_message(channel, msg)
+    if (client.user not in bans):
+        msg = '**{0.name}** left or was kicked from the server.'.format(server)
+        await client.send_message(channel, msg)
 
 @client.event
 async def on_member_join(server):
-	msg = '**{0.name}** has joined the server.'.format(server)
-	await client.send_message(channel, msg)
+    msg = '**{0.name}** has joined the server.'.format(server)
+    await client.send_message(channel, msg)
 
 @client.event
 async def on_member_unban(server, user):
-	msg = '**{0.name}** was unbanned.'.format(user)
-	await client.send_message(channel, msg)
-	global bans
-	if (client.user in bans):
-		bans.remove(client.user)
+    msg = '**{0.name}** was unbanned.'.format(user)
+    await client.send_message(channel, msg)
+    global bans
+    if (client.user in bans):
+        bans.remove(client.user)
 
 loop = asyncio.get_event_loop()
 
